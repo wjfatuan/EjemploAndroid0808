2644bfa8fb9a884250b52a5e9f949d81eb89565c
wjfatuan <wilson.forero@uan.edu.co>
2023-08-10 20:35:02 -0500

    Uso de Toast y Textview

--- app/src/main/java/co/edu/uan/android/ejemploandroid0808/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/ejemploandroid0808/MainActivity.kt
@@ -3,29 +3,40 @@ package co.edu.uan.android.ejemploandroid0808
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val num1:Int = 10
-    val num2 = 30
+    val num1:Int = (0..100).random()
+    val num2 = (0..100).random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val b1 = findViewById<Button>(R.id.button1)
+        val tr = findViewById<TextView>(R.id.resultado)
         b1.setOnClickListener {
-            if(num1>num2)
-                println("Felicitaciones, gano")
-            else
-                println(":(")
+            if(num1>num2) {
+                tr.text =     "Felicitaciones, gano"
+                Toast.makeText(this, "Felicitaciones, gano", Toast.LENGTH_LONG).show()
+            }
+            else {
+                tr.text =     "\uD83D\uDE41"
+                Toast.makeText(this, "\uD83D\uDE41", Toast.LENGTH_LONG).show()
+            }
         }
 
         val b2 = findViewById<Button>(R.id.button2)
         b2.setOnClickListener {
-            if(num1<num2)
-                println("Felicitaciones, gano")
-            else
-                println(":(")
+            if(num1<num2) {
+                tr.text =     "Felicitaciones, gano"
+                Toast.makeText(this, "Felicitaciones, gano", Toast.LENGTH_LONG).show()
+            }
+            else {
+                tr.text =     "\uD83D\uDE41"
+                Toast.makeText(this, "\uD83D\uDE41", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
